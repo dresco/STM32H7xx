04d66016c9dce6359ae6face4627a07ad580246a
Jon Escombe <jone@dresco.co.uk>
2022-07-31 10:15:07 +0100

    Add option to override SDMMC clock for prototyping on Nucleo.
    Needs a slower clock when using uSD card breakout with jumper wires

--- Src/sdmmc.c
+++ Src/sdmmc.c
@@ -23,6 +23,7 @@
 
 */
 
+#include "driver.h"
 
 #if SDCARD_ENABLE
 
--- Src/stm32h7xx_hal_msp.c
+++ Src/stm32h7xx_hal_msp.c
@@ -96,6 +96,22 @@ void HAL_SD_MspInit(SD_HandleTypeDef* hsd)
   */
     PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_SDMMC;
     PeriphClkInitStruct.SdmmcClockSelection = RCC_SDMMCCLKSOURCE_PLL;
+
+
+#ifdef NUCLEO_SLOW_SDMMC_CLOCK
+    // Select alternative SDMMC clock source for prototyping on Nucleo with external uSD breakout. A slower
+    // clock is needed for signal integrity (note that changing the ClockDiv does not have the same effect).
+    PeriphClkInitStruct.PLL2.PLL2M = 2;
+    PeriphClkInitStruct.PLL2.PLL2N = 240;
+    PeriphClkInitStruct.PLL2.PLL2P = 2;
+    PeriphClkInitStruct.PLL2.PLL2Q = 2;
+    PeriphClkInitStruct.PLL2.PLL2R = 80;
+    PeriphClkInitStruct.PLL2.PLL2RGE = RCC_PLL2VCIRANGE_2;
+    PeriphClkInitStruct.PLL2.PLL2VCOSEL = RCC_PLL2VCOWIDE;
+    PeriphClkInitStruct.PLL2.PLL2FRACN = 0;
+    PeriphClkInitStruct.SdmmcClockSelection = RCC_SDMMCCLKSOURCE_PLL2;
+#endif
+
     if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK)
     {
       Error_Handler();
