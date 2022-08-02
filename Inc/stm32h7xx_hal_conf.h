2856e0991513aa28ebc3ec6e16510a401d606fb9
Jon Escombe <jone@dresco.co.uk>
2022-08-02 14:11:48 +0100

    Increase Ethernet Tx DMA descriptors for http server
    Due to the way http responses are formed with dynamic headers, we
    need one DMA descriptor for each header + and one for the data.
    
    If we attempt to use more than defined, the packet is never sent.

--- Inc/stm32h7xx_hal_conf.h
+++ Inc/stm32h7xx_hal_conf.h
@@ -220,7 +220,7 @@
 #define  USE_HAL_WWDG_REGISTER_CALLBACKS    0U /* WWDG register callback disabled    */
 
 /* ########################### Ethernet Configuration ######################### */
-#define ETH_TX_DESC_CNT         4  /* number of Ethernet Tx DMA descriptors */
+#define ETH_TX_DESC_CNT        12  /* number of Ethernet Tx DMA descriptors */
 #define ETH_RX_DESC_CNT         4  /* number of Ethernet Rx DMA descriptors */
 
 #define ETH_MAC_ADDR0    (0x02UL)
