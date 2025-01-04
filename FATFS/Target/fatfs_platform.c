33de64f0fe40e22af82bdce885aba0d10cef8387
Jon Escombe <jone@dresco.co.uk>
2025-01-04 18:00:21 +0000

    Remove incorrect SD card detection code
    (likely imported from STM32Cube project)

--- FATFS/Target/fatfs_platform.c
+++ FATFS/Target/fatfs_platform.c
@@ -18,13 +18,9 @@
 /* USER CODE END Header */
 #include "fatfs_platform.h"
 
-uint8_t	BSP_PlatformIsDetected(void) {
+uint8_t BSP_PlatformIsDetected(void) {
     uint8_t status = SD_PRESENT;
-    /* Check SD card detect pin */
-    if(HAL_GPIO_ReadPin(SD_DETECT_GPIO_PORT, SD_DETECT_PIN) != GPIO_PIN_RESET)
-    {
-        status = SD_NOT_PRESENT;
-    }
+
     /* USER CODE BEGIN 1 */
     /* user code can be inserted here */
     /* USER CODE END 1 */
--- FATFS/Target/fatfs_platform.h
+++ FATFS/Target/fatfs_platform.h
@@ -21,7 +21,5 @@
 /* Defines ------------------------------------------------------------------*/
 #define SD_PRESENT               ((uint8_t)0x01)  /* also in bsp_driver_sd.h */
 #define SD_NOT_PRESENT           ((uint8_t)0x00)  /* also in bsp_driver_sd.h */
-#define SD_DETECT_PIN         GPIO_PIN_4
-#define SD_DETECT_GPIO_PORT   GPIOD
 /* Prototypes ---------------------------------------------------------------*/
-uint8_t	BSP_PlatformIsDetected(void);
+uint8_t BSP_PlatformIsDetected(void);
