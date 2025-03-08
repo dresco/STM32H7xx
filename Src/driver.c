071149beb34cb2895105fddbdd1ebbdecec5d9f1
Jon Escombe <jone@dresco.co.uk>
2025-03-08 12:03:14 +0000

    Updated for networking library changes

--- Src/driver.c
+++ Src/driver.c
@@ -2598,7 +2598,7 @@ bool driver_init (void)
 #else
     hal.info = "STM32H743";
 #endif
-    hal.driver_version = "250228";
+    hal.driver_version = "250303";
     hal.driver_url = "https://github.com/dresco/STM32H7xx";
 #ifdef BOARD_NAME
     hal.board = BOARD_NAME;
